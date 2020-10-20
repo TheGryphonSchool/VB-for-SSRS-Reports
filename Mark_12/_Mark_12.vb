9b1d709e2a643bee35fb3e046f3e928d6799c253
ZacTredger <zac4793@gmail.com>
2020-10-20 20:46:08 +0100

    Include arrays utilitites in Mark_12 (SLT version)

--- Mark_12/_Mark_12.vb
+++ Mark_12/_Mark_12.vb
@@ -63,6 +63,40 @@ Public Function colourFromScale(fraction As Double, _
     Return header_colour_scale.getColour(fraction)
 End Function
 
+'C:\USERS\ZAC\DOCUMENTS\PROJECTS\SSRS CODE\UTILITIES\ARRAYS.VB
+Public Function removeDuplicates(items As Object()) As Object()
+    'maps original indicies to new indicies
+    Dim index_map As New _
+        System.Collections.Generic.Dictionary(Of Integer, Integer)
+    Dim shift As Integer = 0
+    Dim unique_items As New System.Collections.Generic.List(Of Object)
+    Dim index As Integer = 0
+    For Each item As Object In items
+        If unique_items.contains(item) Then
+            shift += 1
+        Else
+            unique_items.Add(item)
+            index_map.Add(index, index - shift)
+        End If
+        index += 1
+    Next item
+    For Each old_index As Integer In index_map.Keys
+        items(index_map(old_index)) = items(old_index)
+    Next old_index
+    ReDim Preserve items(index - 1 - shift)
+    Return items
+End Function
+
+Public Function SumArray(nums() As Object) As Integer
+    Dim num As Integer
+	SumArray = 0
+	For Each o As Object In nums
+        If Integer.TryParse(o, num)
+            SumArray += num
+        End If
+	Next o
+End Function
+
 'C:\USERS\ZAC\DOCUMENTS\PROJECTS\SSRS CODE\UTILITIES\LOOKUP_PARAMS.VB
 Public NotInheritable Class ParamLookups
     Private Shared singleton_instance As ParamLookups
