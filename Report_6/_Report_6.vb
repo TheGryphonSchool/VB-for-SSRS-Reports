c1697fdc66207093c1a9bc7e91089eec7895188a
ZacTredger <zac4793@gmail.com>
2020-10-22 13:21:30 +0100

    Get effective marks from string of delimited marks
    Use for sorting ranks. At the moment, there won't be any delimited marks, but
    it works with or without them.

--- Report_6/_Report_6.vb
+++ Report_6/_Report_6.vb
@@ -7,6 +7,34 @@ Public Function roundIfFloat(float As String) As String
     Return CStr(CInt(float))
 End Function
 
+''' <summary>
+'''     Calculate the average value of a series of 0 or more values in a string
+''' </summary>
+''' <param name="vals">
+'''     A string containing 0 or more numeric values delimited by `, ` 
+''' </param>
+''' <returns>
+'''     The average of <c>vals</c> as a double, or 40.0 if <c>vals</c> is empty
+''' </returns>
+Public Function EffectiveMark(vals As String, _
+                              Optional valIfBlank As Double = 40) As Double
+    Dim current As Double
+    Dim sum As Double = 0
+    Dim count As Integer = 0
+
+    If vals = "" Then
+        Return 0
+    End If
+    For Each val As String In Split(vals, ", ")
+        If not Double.TryParse(val, current) Then
+            return valIfBlank
+        End If
+        sum += current
+        count += 1
+    Next val
+    Return sum / count
+End Function
+
 'C:\USERS\ZAC\DOCUMENTS\PROJECTS\SSRS CODE\UTILITIES\ARRAYS.VB
 Public Function removeDuplicates(items As Object()) As Object()
     'maps original indicies to new indicies
--- Report_6/miscellaneous.vb
+++ Report_6/miscellaneous.vb
@@ -4,3 +4,31 @@ Public Function roundIfFloat(float As String) As String
     End If
     Return CStr(CInt(float))
 End Function
+
+''' <summary>
+'''     Calculate the average value of a series of 0 or more values in a string
+''' </summary>
+''' <param name="vals">
+'''     A string containing 0 or more numeric values delimited by `, ` 
+''' </param>
+''' <returns>
+'''     The average of <c>vals</c> as a double, or 40.0 if <c>vals</c> is empty
+''' </returns>
+Public Function EffectiveMark(vals As String, _
+                              Optional valIfBlank As Double = 40) As Double
+    Dim current As Double
+    Dim sum As Double = 0
+    Dim count As Integer = 0
+
+    If vals = "" Then
+        Return 0
+    End If
+    For Each val As String In Split(vals, ", ")
+        If not Double.TryParse(val, current) Then
+            return valIfBlank
+        End If
+        sum += current
+        count += 1
+    Next val
+    Return sum / count
+End Function
