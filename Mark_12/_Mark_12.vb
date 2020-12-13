9fadcb2feee69d63300aabfdaa9358bb8a1c598e
ZacTredger <zac4793@gmail.com>
2020-12-13 21:26:08 +0000

    Fix M12 science/double-science bug
    Except for GCSE Triple Science and A Level Sciences, learners ARE in
    multiple groups for science, violating one of the assumptions behind the
    SLT & HoD M12 reports. This caused all the grades to be attributed to
    the first group, (and none to subsequent groups).
    
    I fixed this using a custom function that checks whether the subject is
    science, and if so, looks among the matching leaner-subjects in the
    lookup for a group with the same denomination (e.g. Bi/Ch/Ph).

--- Mark_12/_Mark_12.vb
+++ Mark_12/_Mark_12.vb
@@ -1,6 +1,42 @@
 'File produced by combining files using the Combine Files VScode extension
 'C:\USERS\ZAC\DOCUMENTS\PROJECTS\SSRS CODE\MARK_12\MISCELLANEOUS.VB
-' Blank
+''' <summary>
+    ''' If the subject is science or double science, the relevant group
+    ''' is found carefully by ensuring that it is the same discipline
+    ''' (e.g. discipline `Ph` matches between `11Ph/Gauss` and
+    ''' `10Ph/Euler`). If the subject is not a generic science, a normal
+    ''' lookup will be used.
+''' </summary>
+''' <param name="param">
+    ''' An SSRS parameter in this form:
+    ''' Value: [learnerID][delimeter][subjectCode]
+    ''' Label: [groupCode]
+''' </param>
+''' <returns>
+'''     The group code that a learner is currently in for a subject
+''' </returns>
+Public Function LookupGroupScientifically(learnerID As String, _
+                                            subjectCode As String, _
+                                            groupCode As String, _
+                                            param As Object, _
+                                            Optional matchStrategy As Char = "E"C _
+                                            ) As Object
+    If subjectCode <> "Sc" AndAlso subjectCode <> "ScDouble" Then
+        Return LookupParam("value", learnerID & subjectCode, param, _
+                            matchStrategy, True)
+    End If
+
+    Dim subSubject As String = New Text.RegularExpressions. _
+        Regex("^\d{1,2}(\w+)(?=\/)").Match(groupCode).Groups(1).Value
+    Dim groupCodeRegex As Text.RegularExpressions.Regex = New _
+        Text.RegularExpressions.Regex("^\d{1,2}" & subSubject & "\/")
+    For Each potentialGroup As Object In _
+        LookupAllMatchingParams("value", learnerID & subjectCode, _
+                                param, matchStrategy)
+        If groupCodeRegex.IsMatch(potentialGroup.ToString()) Then Return potentialGroup
+    Next potentialGroup
+    Return Nothing
+End Function
 'C:\USERS\ZAC\DOCUMENTS\PROJECTS\SSRS CODE\UTILITIES\COLOUR_SCALE.VB
 Public Class ColourScale
     Private scale As New System.Collections.Generic.List(Of Integer())
